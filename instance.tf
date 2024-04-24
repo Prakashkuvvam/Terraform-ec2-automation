644b95061f298c29cf29404a5ab470c48029df11
prakash <prakash@mymail.com>
2024-04-24 20:50:18 +0530

    fixed error in ec2 tf

--- instance.tf
+++ instance.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "myec2" {
   instance_type               = var.instance-type
   associate_public_ip_address = true
   key_name                    = "sample"
+  subnet_id                   = aws_subnet.mysub.id
   vpc_security_group_ids      = [aws_security_group.mySG.id]
   tags = {
     "Name" = "Google-Instane"
