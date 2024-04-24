7088509fa6a5cb1c322620959f00f8e18682caa5
prakash <prakash@mymail.com>
2024-04-24 20:45:14 +0530

    fmt on ec2 tf

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "myec2" {
-  ami = var.ami
-  instance_type = var.instance-type
+  ami                         = var.ami
+  instance_type               = var.instance-type
   associate_public_ip_address = true
-  key_name = "sample"
-  vpc_security_group_ids = [aws_security_group.mySG.id]
+  key_name                    = "sample"
+  vpc_security_group_ids      = [aws_security_group.mySG.id]
   tags = {
     "Name" = "Google-Instane"
   }
